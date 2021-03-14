cb0446c7418d29380855fab2d69d66587459f7fe
Ariukae <80608978+Ariukae@users.noreply.github.com>
2021-03-14 09:32:24 +0500

    Add files via upload

---
+++ #1.2 Homework.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout<<"Whether you want to uncover the secrets of the universe,"<<endl;
+    cout<<"or you just want to pursue a career in the 21st century,"<<endl;
+	cout<<"basic computer programming is an essential skill to learn."<<endl;
+	cout<<"-[Stephen Hawking]."<<endl;
+
+}
