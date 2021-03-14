c534d2e151ad4d081d243e38b91e27917339e28a
Ariukae <80608978+Ariukae@users.noreply.github.com>
2021-03-14 09:28:40 +0500

    Add files via upload

---
+++ #1 Homework.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int x=1,y=2,z=3;
+	
+	cout<<"1. "<<x+y-z<<endl;
+	cout<<"2. "<<x/z*y<<endl;
+	cout<<"3. "<<20*x*(z+y)<<endl;
+	return 0;
+	
+}
