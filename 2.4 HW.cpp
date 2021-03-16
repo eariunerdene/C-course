1cca4ed725b232b736bf6ea924badcc53c94f220
Ariukae <80608978+Ariukae@users.noreply.github.com>
2021-03-16 09:43:39 +0500

    Add files via upload

---
+++ 2.4 HW.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Tegsh ontsogtiin urt bolon orgoniig oruulna uu"<<endl;
+	cin>>a>>b;
+	cout<<"Tegsh ontsogtiin P="<<(a+b)*2<<endl;
+	cout<<"Tegsh ontsogtiin S="<<a*b<<endl;
+}
+
