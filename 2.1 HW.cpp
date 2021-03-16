4f9c691d24e3c88c1df2d8799ae0d622bcd78177
Ariukae <80608978+Ariukae@users.noreply.github.com>
2021-03-16 09:32:19 +0500

    Add files via upload

---
+++ 2.1 HW.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int a;	
+	cout<<"buhel torliin input ee oruulna uu"<<endl;
+	cin>>a;
+	cout<<"tanii oruulsan buhel torliin too: "<<a<<endl;
+	char b;
+	cout <<"Temdegt torliin input ee oruulna uu"<<endl;
+	cin>>b;
+	cout<<"tanii oruulsan temdegt: "<<b<<endl;
+	string utga;
+	cout <<"Ta temdegtuudiin tsugluulgiig oruulna uu"<<endl;
+	cin>>utga;
+	cout<<"Tanii oruulsan temdegtuudiin tsugluulga: "<<utga<<endl;
+	bool unen=true;
+	cout<<"unen eseh utga: "<<unen<<endl;
+	double c;
+	cout <<"Ta butarhai torliin utga oruulna uu"<<endl;
+	cin>>c;
+	cout<<"Tanii oruulsan butarhai torliin utga: "<<c<<endl;
+	float d;
+	cout <<"Ta bogino butarhai torliin oruulna uu"<<endl;
+	cin>>d;
+	cout<<"Tanii oruulsan bogino butarhai torliin utga: "<<d<<endl;
+	return 0;
+}
