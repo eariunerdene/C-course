e1cad3b8b3db76aec5b5d7124928534bb7077327
Ariukae <80608978+Ariukae@users.noreply.github.com>
2021-03-16 09:37:16 +0500

    Add files via upload

---
+++ 2.2 HW.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x,y;
+	cout<<"2 toogoo oruulna uu"<<endl;
+	cin>>x>>y;
+	cout<<"2 toonii niilber= "<<x+y<<endl;
+	cout<<"2 toonii ylgavar= "<<x-y<<endl;
+	cout<<"2 toonii urjver= "<<x*y<<endl;
+	cout<<"2 toonii noogdvor= "<<x/y<<endl;
+	return 0;
+}
