450c66c0f3659f647ab86ebc65ca88841f035582
Ariukae <80608978+Ariukae@users.noreply.github.com>
2021-03-16 09:55:41 +0500

    Add files via upload

---
+++ 2.5 HW.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float r;
+	cout<<"Toirgiin R-g oruulna uu"<<endl;
+	cin>>r;
+	cout<<"Toirgiin Diameter ni= "<<2*r<<endl;
+	cout<<"Toirgiin perimetr ni= "<<2*3.14*r<<endl;
+	cout<<"Toirgiin talbai ni= "<<2*3.14*r*r<<endl;
+	return 0;
+}
