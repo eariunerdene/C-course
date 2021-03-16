5affca85ea79a1cce68d663ee78c00662b83ba0e
Ariukae <80608978+Ariukae@users.noreply.github.com>
2021-03-16 10:01:58 +0500

    Add files via upload

---
+++ 2.6 HW.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a;
+	cout<<"Urtiig santimeteer oruulna uu"<<endl;
+	cin>>a;
+	cout<<"M= "<<a/100<<endl;
+	cout<<"KM= "<<a/100000<<endl;
+	return 0;
+}
