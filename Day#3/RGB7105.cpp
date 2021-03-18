072442af0b07e32c73e71530d63d407fbb5d87ae
Ariuka Enkhbat <80608978+Ariukae@users.noreply.github.com>
2021-03-18 11:33:06 +0800

    Add files via upload

---
+++ Day#3/RGB7105.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d; // 100 80 50 148
+	cin>>a>>b>>c>>d;
+	if (a<80) 
+		a=0;
+	if (b<80) 
+		b=0;
+	if (c<80)
+		c=0;
+	if (d<80)
+	cout>>a+b+c+d
+}
