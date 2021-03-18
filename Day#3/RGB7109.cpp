a15b035dc485dac844f21dfa29a732351cf39de7
Ariuka Enkhbat <80608978+Ariukae@users.noreply.github.com>
2021-03-18 11:56:13 +0800

    Add files via upload

---
+++ Day#3/RGB7109.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int a,b,c,tool;
+	cin>>a>>b>>c;
+	if (a==5) tool+=1;
+	if (b==5) tool+=1;
+	if (c==5) tool+=1;
+	cout<<tool;
+	return 0;
+}
