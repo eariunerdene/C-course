6b68f8c1f95656c8e777b3afc1fa4d7cd2c49f01
Ariuka Enkhbat <80608978+Ariukae@users.noreply.github.com>
2021-03-18 12:29:34 +0800

    Add files via upload

---
+++ Day#3/RGB7114.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cin>>a>>b>>c;
+	if (a%2==0) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+	if (b%2==0) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+	if (c%2==0) cout<<"YES"<<endl;
+	else cout<<"NO"<<endl;
+}
