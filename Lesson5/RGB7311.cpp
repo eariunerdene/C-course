6b12dbabf816c90f2a36f30b8cd65fccd438f5e0
Ariuka Enkhbat <80608978+eariunerdene@users.noreply.github.com>
2021-03-24 10:48:48 +0800

    Add files via upload

---
+++ Lesson5/RGB7311.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	/*
+	Ogogdson toog 3-n zeregted huvaa
+	input:36
+	output:4
+	*/
+	int n;
+	cin>>n;
+	for(;n%3==0;n/=3){
+	}
+		cout<<n;
+}
