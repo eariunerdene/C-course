07cb42cd20dd21d0bb81a320c8c0ff394a557bc2
Ariuka Enkhbat <80608978+eariunerdene@users.noreply.github.com>
2021-03-24 10:42:19 +0800

    Add files via upload

---
+++ Lesson5/RGB7310.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	/*
+	Ogogdson too 2-n zeregt bol YES ugui bol NO
+	input:16
+	output:YES
+	*/
+	int n;
+	cin>>n;
+	for(;(n%2==0);n/=2){
+		}
+		if(n==1)
+		cout<<"YES";
+		else
+		cout<<"NO";
+}
